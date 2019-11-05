9cb9a22d326873f87f588b7048cb9982dcc20b9c
Daniel Weiß <wanieldeiss@gmail.com>
2019-11-05 05:40:31 +0100

    handle if email is unknowen

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -12,18 +12,19 @@ export class AuthService {
   ) {}
 
   async validateUserByPassword(loginAttempt: LoginUserDto) {
-    // This will be used for the initial login
     let userToAttempt: any = await this.usersService.findOneByEmail(
       loginAttempt.email,
     );
 
+    if (!userToAttempt) {
+      throw new UnauthorizedException();
+    }
+
     return new Promise(resolve => {
-      // Check the supplied password against the hash stored for this email address
       userToAttempt.checkPassword(loginAttempt.password, (err, isMatch) => {
         if (err) throw new UnauthorizedException();
 
         if (isMatch) {
-          // If there is a successful match, generate a JWT for the user
           resolve(this.createJwtPayload(userToAttempt));
         } else {
           throw new UnauthorizedException();
@@ -33,7 +34,6 @@ export class AuthService {
   }
 
   async validateUserByJwt(payload: JwtPayload) {
-    // This will be used when the user has already logged in and has a JWT
     let user = await this.usersService.findOneByEmail(payload.email);
 
     if (user) {
