2a6b609900d8d3086a5cf280f57511c0fb079d85
Daniel Weiß <wanieldeiss@gmail.com>
2019-12-13 06:55:29 +0100

    add players to archive schema, disable unique player name #61

--- src/highscore/schemas/archive.schema.ts
+++ src/highscore/schemas/archive.schema.ts
@@ -1,10 +1,11 @@
 import * as mongoose from 'mongoose';
 import { Archive } from '../interfaces/archive.interface';
+import { PlayerSchema } from './player.schema';
 
 export const ArchiveSchema: mongoose.Schema = new mongoose.Schema({
   name: String,
   year: Number,
-  players: Array,
+  players: [PlayerSchema],
 });
 
 export interface IArchiveModel extends Archive, mongoose.Document {}
