1725aad67be58e9f2e1a7824c20764655ff1d388
Daniel Weiß <wanieldeiss@gmail.com>
2020-06-24 08:48:42 +0200

    make client work again

--- client/src/app/components/footer/footer.component.ts
+++ client/src/app/components/footer/footer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { version } from '../../../../package.json';
+import { version } from '../../../../../package.json';
 import { ArchiveService } from '@app/services/archive.service.js';
 
 @Component({
